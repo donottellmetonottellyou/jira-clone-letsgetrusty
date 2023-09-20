769ce3986f5584b50daafd9a707f7c4cd5e9f4ea
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2023-09-20 15:37:41 -0400

    started step 2 of stage 3

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ mod models;
 mod navigator;
 mod ui;
 
+use db::*;
+use io_utils::*;
+use navigator::*;
+
+use std::rc::Rc;
+
 fn main() {
-    println!("Welcome To My-Jira!");
+    // TODO: create database and navigator
+
+    loop {
+        clearscreen::clear().unwrap();
+
+        // TODO: implement the following functionality:
+        // 1. get current page from navigator. If there is no current page exit the loop.
+        // 2. render page
+        // 3. get user input
+        // 4. pass input to page's input handler
+        // 5. if the page's input handler returns an action let the navigator process the action
+    }
 }
